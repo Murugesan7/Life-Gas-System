3289a00852306bfbe15c4edd923ffbf510deb80d
Murugesan7 <naanjil.murugesan@gmail.com>
2024-01-31 16:03:04 +0530

    second image is edited

--- src/Components/ProductContainer/ProductContainer.js
+++ src/Components/ProductContainer/ProductContainer.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Element } from 'react-scroll';
-import product from  "../img/p1.jpg"
+// import product from  "../img/p1.jpg"
 
 const ProductContainer = () => {
   return (
     <Element className="productcontainer" id="products">
       <div className='productcontainer_image'>
-        <img src={product} alt=''></img>
+        {/* <img src={product} alt=''></img> */}
       </div>
     </Element>
   )
